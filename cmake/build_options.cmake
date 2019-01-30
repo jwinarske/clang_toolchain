4692ea5693dd4d8df0480186c98cc1ed62e4e716
Joel Winarske <joel.winarske@gmail.com>
2019-01-30 15:38:46 +0000

    default binutils on, patch binutils, add newlib (not working yet)

--- cmake/build_options.cmake
+++ cmake/build_options.cmake
@@ -1,10 +1,20 @@
 
+#
+# Sysroot
+#
 option(BUILD_RPI_SYSROOT "Build Raspberry Pi sysroot" ON)
 
+#
+# Host
+#
 option(BUILD_LLD "Checkout and build LLVM Linker for host" ON)
-option(BUILD_BINUTILS "Download and build binutils for host" OFF)
+option(BUILD_BINUTILS "Download and build binutils for host" ON)
 option(BUILD_LLDB "Checkout and build LLVM Debugger for host" OFF)
 
+#
+# Target
+#
+option(BUILD_NEWLIB "Checkout and build newlib" OFF)
 option(BUILD_COMPILER_RT "Checkout and build compiler-rt" ON)
 option(BUILD_LIBCXXABI "Checkout and build libcxxabi for target" ON)
 option(BUILD_LIBUNWIND "Checkout and build libunwind for  target" ON)
