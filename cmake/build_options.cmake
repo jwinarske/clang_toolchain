93d96e193dc95c66545833a83ed9bdae452e1c27
Joel Winarske <joel.winarske@gmail.com>
2019-11-12 20:53:36 -0800

    sysroot cleanup

--- cmake/build_options.cmake
+++ cmake/build_options.cmake
@@ -4,6 +4,7 @@
 #
 option(BUILD_PLATFORM_SYSROOT "Build Platform sysroot" OFF)
 option(BUILD_PLATFORM_RPI "Build Platform Raspberry Pi" OFF)
+option(UPDATE_TARGET "Update Target libraries" OFF)
 
 #
 # Host
@@ -27,3 +28,16 @@ option(BUILD_LIBCXX "Checkout and build libcxx for target" ON)
 #
 option(BUILD_WAYLAND "Build Wayland apps" OFF)
 option(BUILD_MRAA "Build MRAA repo" OFF)
+
+#
+# Sysroot
+#
+
+# Shop for Raspbian rootfs releases here: https://downloads.raspberrypi.org/raspbian/archive/
+if(NOT ROOTFS_ARCHIVE_VERSION)
+    set(ROOTFS_ARCHIVE_VERSION 2019-09-30-15:24)
+endif()
+
+if(NOT ROOTFS_ARCHIVE_MD5)
+    set(ROOTFS_ARCHIVE_MD5 766f46bb5d3a223f8a8329393635f175)
+endif()
